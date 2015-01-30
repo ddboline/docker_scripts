16e819731e20016fec19adaed5f8853edcb7c00c
Daniel Boline <ddboline@gmail.com>
2015-01-30 09:33:23 -0500

    bug fixes

--- check_automated_test.sh
+++ check_automated_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo echo ""
-CONTAINERS=`sudo docker ps -a | awk '/CONTAIN/ {print $1}'`
+CONTAINERS=`sudo docker ps -a | awk '!/CONTAIN/ {print $1}'`
 
 for C in $CONTAINERS;
 do
-    sudo docker logs $C 2>&1 | tail -n10
+    sudo docker logs $C 2>&1 | tail -n2
 done
