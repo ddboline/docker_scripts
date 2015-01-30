b12270fe1194d21e529785765deee36f7762e929
Daniel Boline <ddboline@gmail.com>
2015-01-30 09:43:03 -0500

    ...

--- dump_automated_test_results.sh
+++ dump_automated_test_results.sh
@@ -4,3 +4,9 @@ sudo echo ""
 sudo docker ps -a
 
 CONTAINERS=`sudo docker ps -a | awk '!/CONTAIN/ {print $1}'`
+
+for C in $CONTAINERS;
+do
+    mkdir temp_$C
+    sudo docker cp ${C}:/root temp_${C}/
+done
