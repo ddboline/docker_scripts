085d04396f7c8196506b8fa7213dfdf1e3d59a02
Daniel Boline <ddboline@gmail.com>
2015-01-30 11:25:12 -0500

    ...

--- dump_automated_test_results.sh
+++ dump_automated_test_results.sh
@@ -8,6 +8,6 @@ CONTAINERS=`sudo docker ps -a | awk '!/CONTAIN/ {print $1}'`
 for C in $CONTAINERS;
 do
     mkdir temp_$C
-    sudo docker cp ${C}:/root temp_${C}/
+    sudo docker cp ${C}:/root/output.* temp_${C}/
     sudo chown -R ${USER}:${USER} temp_${C}/
 done
