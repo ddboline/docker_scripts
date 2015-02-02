c6bc2f61156512ba20cbe1371b4ea8410d033794
Daniel Boline <ddboline@gmail.com>
2015-02-02 06:58:21 -0600

    datestamp to output

--- dump_automated_test_results.sh
+++ dump_automated_test_results.sh
@@ -4,11 +4,12 @@ sudo echo ""
 sudo docker ps -a
 
 CONTAINERS=`sudo docker ps -a | awk '!/CONTAIN/ {print $1}'`
+D=`date +%Y%m%d%H%M%S`
 
 for C in $CONTAINERS;
 do
     mkdir temp_$C
-    sudo docker cp ${C}:/root/output.out temp_${C}/
-    sudo docker cp ${C}:/root/output.err temp_${C}/
-    sudo chown -R ${USER}:${USER} temp_${C}/
+    sudo docker cp ${C}:/root/output.out temp_${D}_${C}/
+    sudo docker cp ${C}:/root/output.err temp_${D}_${C}/
+    sudo chown -R ${USER}:${USER} temp_${D}_${C}/
 done
