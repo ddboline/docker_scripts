229b39d050d96b6e02b171df56ca1c27fab87951
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-19 20:20:47 -0500

    ...

--- dump_automated_test_results.sh
+++ dump_automated_test_results.sh
@@ -3,14 +3,16 @@
 sudo echo ""
 sudo docker ps -a
 
-CONTAINERS=`sudo docker ps -a | awk '!/CONTAIN/ {print $1}'`
+CONTAINERS=`sudo docker ps -a | awk '!/CONTAIN/ {printf("%s,%s\n", $1,$7}'`
 D=`date +%Y%m%d%H%M%S`
 
-for C in $CONTAINERS;
+for CN in $CONTAINERS;
 do
-    mkdir temp_${D}_${C}
-    sudo docker cp ${C}:/root/output.out temp_${D}_${C}/
-    sudo docker cp ${C}:/root/output.err temp_${D}_${C}/
+    C=`echo $CN | sed 's:,: :g' | awk '{print $1}'`
+    N=`echo $CN | sed 's:,: :g' | awk '{print $1}'`
+    mkdir temp_${N}_${D}_${C}
+    sudo docker cp ${C}:/root/output.out temp_${N}_${D}_${C}/
+    sudo docker cp ${C}:/root/output.err temp_${N}_${D}_${C}/
     #sudo docker cp ${C}:/root/garmin_app temp_${D}_${C}/
-    sudo chown -R ${USER}:${USER} temp_${D}_${C}/
+    sudo chown -R ${USER}:${USER} temp_${N}_${D}_${C}/
 done
--- run_automated_test.sh
+++ run_automated_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo echo ""
+D=`date +%Y%m%d%H%M%S`
 for REPO in aws_scripts gapi_scripts garmin_app programming_tests;
 do
-    sudo docker run -d ddboline/ddboline_keys:v0.4 /root/run_testing.sh ${REPO}
+    sudo docker run -d ddboline/ddboline_keys:v0.4 --name="${REPO}_${D}" /root/run_testing.sh ${REPO}
 done
 
-sudo docker run -d ddboline/ddboline_keys:v0.4 /root/run_testing_ssh.sh ddboline_personal_scripts
+sudo docker run -d ddboline/ddboline_keys:v0.4 --name="ddboline_personal_scripts_${D}" /root/run_testing_ssh.sh ddboline_personal_scripts
