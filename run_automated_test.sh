0978068eb20a45b2640c07bedec034ead1f0ad10
Daniel Boline <ddboline@gmail.com>
2015-03-01 22:44:15 -0500

    v0.4->latest

--- run_automated_test.sh
+++ run_automated_test.sh
@@ -4,7 +4,7 @@ sudo echo ""
 D=`date +%Y%m%d%H%M%S`
 for REPO in aws_scripts gapi_scripts garmin_app programming_tests;
 do
-    sudo docker run --name=\"${REPO}_${D}\" -d ddboline/ddboline_keys:v0.4 /root/run_testing.sh ${REPO}
+    sudo docker run --name=\"${REPO}_${D}\" -d ddboline/ddboline_keys:latest /root/run_testing.sh ${REPO}
 done
 
-sudo docker run --name=\"ddboline_personal_scripts_${D}\" -d ddboline/ddboline_keys:v0.4 /root/run_testing_ssh.sh ddboline_personal_scripts
+sudo docker run --name=\"ddboline_personal_scripts_${D}\" -d ddboline/ddboline_keys:latest /root/run_testing_ssh.sh ddboline_personal_scripts
