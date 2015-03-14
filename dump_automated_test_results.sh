5d9ae6c4e132c58be0d5ad3e2c180ee0537a7826
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-13 21:44:58 -0400

    ...

--- dump_automated_test_results.sh
+++ dump_automated_test_results.sh
@@ -11,8 +11,10 @@ do
     C=`echo $CN | sed 's:,: :g' | awk '{print $1}'`
     N=`echo $CN | sed 's:,: :g' | awk '{print $2}'`
     mkdir temp_${N}_${D}_${C}
-    sudo docker cp ${C}:/root/output*.out temp_${N}_${D}_${C}/
-    sudo docker cp ${C}:/root/output*.err temp_${N}_${D}_${C}/
+    sudo docker cp ${C}:/root/output.out temp_${N}_${D}_${C}/
+    sudo docker cp ${C}:/root/output.err temp_${N}_${D}_${C}/
+    sudo docker cp ${C}:/root/*/output*.out temp_${N}_${D}_${C}/
+    sudo docker cp ${C}:/root/*/output*.err temp_${N}_${D}_${C}/
     #sudo docker cp ${C}:/root/garmin_app temp_${D}_${C}/
     sudo chown -R ${USER}:${USER} temp_${N}_${D}_${C}/
 done
