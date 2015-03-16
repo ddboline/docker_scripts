38b607f923cc0ad65ae39e40541c92d076ed05cb
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-16 18:04:56 -0400

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -6,4 +6,4 @@ pip install py2deb
 mkdir -p /root/py2deb
 py2deb -r /root/py2deb -- --upgrade $@
 
-scp /root/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/
+# scp /root/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/new/
