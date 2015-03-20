b5d644df7e042f1459f86e715707257d07c73fe2
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-20 19:04:49 -0400

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+REPOS="
+blaze cvxopt nolearn numpy into odo pandas
+scikit-learn scipy theano pystruct sqlalchemy
+gensim nltk spacy scikit-monaco
+pymc
+seaborn
+git+https://github.com/benanne/Lasagne.git
+git+https://github.com/ddboline/pylearn2.git
+"
+
 apt-get update
 apt-get install -y python-pip python-dev
 pip install py2deb
 mkdir -p /root/py2deb
-py2deb -r /root/py2deb -y -- --upgrade $@
+for REPO in $REPOS;
+do
+    py2deb -r /root/py2deb -y -- --upgrade $REPO
+done
 
 # scp /root/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/new/
