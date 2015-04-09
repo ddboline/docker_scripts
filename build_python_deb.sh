bd15f5cfe9f3c292d31319e611dabdba8ccd16df
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-09 15:46:46 -0400

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+# REPOS="
+# numpy scipy pandas 
+# blaze cvxopt scikit-learn
+# into odo pymc seaborn
+# nolearn theano pystruct sqlalchemy
+# gensim nltk spacy scikit-monaco
+# git+https://github.com/benanne/Lasagne.git
+# git+https://github.com/ddboline/pylearn2.git
+# "
 REPOS="
-numpy scipy pandas 
-blaze cvxopt scikit-learn
-into odo pymc seaborn
-nolearn theano pystruct sqlalchemy
-gensim nltk spacy scikit-monaco
-git+https://github.com/benanne/Lasagne.git
-git+https://github.com/ddboline/pylearn2.git
+blaze
 "
 
 apt-get update
-apt-get install -y python-pip python-dev lintian
-apt-get install -y liblapack-dev libblas-dev dpkg-dev
-apt-get install -y gfortran libfreetype6-dev libpng12-dev
-apt-get install -y pkg-config
+apt-get install -y python-pip python-dev lintian liblapack-dev libblas-dev \
+                   dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config
 pip install py2deb
 mkdir -p /root/py2deb
 for REPO in $REPOS;
