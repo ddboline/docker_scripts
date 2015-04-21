de7605d9cefe0fafdf3a66086fa9f95d2e7fdce1
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-21 00:02:23 -0400

    ...

---
+++ build_grive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REPO="https://github.com/Grive/grive.git"
+
+apt-get update
+apt-get install -y libcurl3-dev libjson0-dev libjson-c-dev libjson-glib-dev \
+                   libjsoncpp-dev libyajl-dev binutils-dev libboost-test-dev \
+                   libboost-date-time1.54-dev libboost-filesystem1.54-dev \
+                   libboost-program-options1.54-dev libboost-system1.54-dev \
+                   libboost-thread1.54-dev cmake dpkg-dev libexpat1-dev libqt4-dev
+
+git clone $REPO
+
+cd grive
+
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
