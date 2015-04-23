1fbc97b78d179ade334d8ffdbf8ed418c0df7f4d
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-23 08:29:11 -0400

    ...

--- build_grive.sh
+++ build_grive.sh
@@ -7,10 +7,14 @@ apt-get install -y libcurl3-dev libjson0-dev libjson-c-dev libjson-glib-dev \
                    libjsoncpp-dev libyajl-dev binutils-dev libboost-test-dev \
                    libboost-date-time1.54-dev libboost-filesystem1.54-dev \
                    libboost-program-options1.54-dev libboost-system1.54-dev \
-                   libboost-thread1.54-dev cmake dpkg-dev libexpat1-dev libqt4-dev
+                   libboost-thread1.54-dev cmake dpkg-dev libexpat1-dev libqt4-dev \
+                   checkinstall
 
 git clone $REPO
 
 cd grive
 
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
+make
+### this part is sadly interactive
+checkinstall
