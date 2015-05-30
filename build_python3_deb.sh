a91055b7c647e635f52910ffeb7ecd02cb5e8aa8
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-30 09:25:11 -0400

    new

---
+++ build_python3_deb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# REPOS="
+# blaze numpy scipy pandas cvxopt scikit-learn into odo pymc seaborn 
+# theano pystruct sqlalchemy gensim nltk spacy scikit-monaco
+# git+https://github.com/dnouri/nolearn.git
+# git+https://github.com/benanne/Lasagne.git
+# git+https://github.com/ddboline/pylearn2.git
+# git+https://github.com/fchollet/keras.git
+# git+https://github.com/aigamedev/scikit-neuralnetwork.git
+# "
+REPOS="$@"
+
+apt-get update
+apt-get install -y python-pip python-dev lintian liblapack-dev libblas-dev \
+                   dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config
+pip install py2deb
+mkdir -p /root/py2deb
+for REPO in $REPOS;
+do
+    py2deb -r /root/py2deb -y -- --upgrade $REPO
+done
+
+# scp /root/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/new/
