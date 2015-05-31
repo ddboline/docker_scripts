09a4557a27da6e88e083bef09fd956a888942491
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-31 19:17:41 -0400

    ...

--- build_python3_deb.sh
+++ build_python3_deb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+### hack...
+export LANG="C.UTF-8"
+
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo apt-get update
+
 # REPOS="
 # blaze numpy scipy pandas cvxopt scikit-learn into odo pymc seaborn 
 # theano pystruct sqlalchemy gensim nltk spacy scikit-monaco
@@ -12,9 +18,9 @@
 REPOS="$@"
 
 apt-get update
-apt-get install -y python3-pip python3-dev lintian liblapack-dev libblas-dev \
-                   dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config
-pip3 install py2deb
+apt-get install -y python-pip python-py2deb python3-dev lintian liblapack-dev libblas-dev \
+                   dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
+                   python3-pkg-resources python3-setuptools
 mkdir -p /root/py2deb
 for REPO in $REPOS;
 do
