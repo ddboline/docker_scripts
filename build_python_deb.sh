31b6f7f268cf26c08aeefb6b73072070f8697771
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-04 10:20:15 -0400

    use prebuilt packages rather than building from pip

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -11,10 +11,12 @@
 # "
 REPOS="$@"
 
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
 apt-get update
-apt-get install -y python-pip python-dev lintian liblapack-dev libblas-dev \
-                   dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config
-pip install py2deb
+apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
+                               dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
+                               python-setuptools python-py2deb
+# pip install py2deb
 mkdir -p /root/py2deb
 for REPO in $REPOS;
 do
