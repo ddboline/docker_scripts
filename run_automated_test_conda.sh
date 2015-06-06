7346b51f90757e52236c4666cb52fc724c822c62
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-06 06:02:53 -0400

    new

---
+++ run_automated_test_conda.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SUDO=""
+$SUDO echo ""
+D=`date +%Y%m%d%H%M%S`
+
+if [ -z $1 ]; then
+    REPOS="aws_scripts gapi_scripts garmin_app programming_tests"
+else
+    REPOS="$@"
+fi
+
+for REPO in $REPOS;
+do
+    $SUDO docker run --name=\"${REPO}_${D}\" -d ddboline/ddboline_keys:conda_latest /root/run_testing_conda.sh ${REPO}
+done
---
+++ run_testing_local_conda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+REPONAME=$1
+apt-get update
+apt-get install -y git
+
+export HOME="/root"
+export USER="root"
+
+echo "Host *" >> ${HOME}/.ssh/config
+echo "  StrictHostKeyChecking no" >> ${HOME}/.ssh/config
+
+SETUP_SCRIPT="setup_conda.sh"
+TEST_SCRIPT="test_conda.sh"
+
+cd ${HOME}/${REPONAME}
+sh ${SETUP_SCRIPT}
+sh ${TEST_SCRIPT}
