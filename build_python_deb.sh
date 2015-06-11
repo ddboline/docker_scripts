43901f5abea42e69a2c4db99109d0173044f7766
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-11 14:48:48 -0400

    /home/ubuntu

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -12,15 +12,15 @@
 REPOS="$@"
 
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
-apt-get update
-apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
+sudo apt-get update
+sudo apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
                                dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
                                python-setuptools python-py2deb
 # pip install py2deb
-mkdir -p /root/py2deb
+mkdir -p /home/ubuntu/py2deb
 for REPO in $REPOS;
 do
-    py2deb -r /root/py2deb -y -- --upgrade $REPO
+    py2deb -r /home/ubuntu/py2deb -y -- --upgrade $REPO
 done
 
 # scp /root/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/new/
