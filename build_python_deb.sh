9140061e5f3080edce27a3bcfc1f609309a4d050
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-19 00:14:44 -0400

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# REPOS="
-# blaze numpy scipy pandas cvxopt scikit-learn into odo pymc seaborn 
-# theano pystruct sqlalchemy gensim nltk spacy scikit-monaco
-# git+https://github.com/dnouri/nolearn.git
-# git+https://github.com/benanne/Lasagne.git
-# git+https://github.com/ddboline/pylearn2.git
-# git+https://github.com/fchollet/keras.git
-# git+https://github.com/aigamedev/scikit-neuralnetwork.git
-# "
 REPOS="$@"
 
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
