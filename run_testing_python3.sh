3c2388c8797b2adea5b0b3077296ec45308d8a43
Daniel Boline <ddboline@gmail.com>
2015-08-18 13:32:21 -0400

    ...

---
+++ run_testing_python3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+REPONAME=$1
+sudo apt-get update
+sudo apt-get install -y git
+
+# export HOME="/root"
+# export USER="root"
+
+echo "Host *" >> ${HOME}/.ssh/config
+echo "  StrictHostKeyChecking no" >> ${HOME}/.ssh/config
+
+GITHUB_DIR=${REPONAME}
+GITHUB_REPO="https://github.com/ddboline/${GITHUB_DIR}.git"
+SETUP_SCRIPT="setup_cloud9.sh"
+TEST_SCRIPT="test_cloud9.sh"
+
+git clone ${GITHUB_REPO} ${HOME}/${GITHUB_DIR}
+cd ${HOME}/${GITHUB_DIR}
+sh ${SETUP_SCRIPT}
+sh ${TEST_SCRIPT} 2> ${HOME}/output.err > ${HOME}/output.out
