4a62666179648898d72da5acb9471af8425953b0
Daniel Boline <ddboline@gmail.com>
2015-08-18 13:22:50 -0400

    ...

--- build_python3_deb.sh
+++ build_python3_deb.sh
@@ -5,25 +5,16 @@ export LANG="C.UTF-8"
 
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
 
-# REPOS="
-# blaze numpy scipy pandas cvxopt scikit-learn into odo pymc seaborn 
-# theano pystruct sqlalchemy gensim nltk spacy scikit-monaco
-# git+https://github.com/dnouri/nolearn.git
-# git+https://github.com/benanne/Lasagne.git
-# git+https://github.com/ddboline/pylearn2.git
-# git+https://github.com/fchollet/keras.git
-# git+https://github.com/aigamedev/scikit-neuralnetwork.git
-# "
 REPOS="$@"
 
 sudo apt-get update
-sudo apt-get install -y --force-yes python-pip python-py2deb python3-dev lintian liblapack-dev libblas-dev \
+sudo apt-get install -y --force-yes python3-pip python3-py2deb python3-dev lintian liblapack-dev libblas-dev \
                    dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
                    python3-pkg-resources python3-setuptools
-mkdir -p /home/ubuntu/py2deb
+mkdir -p /home/ubuntu/py2deb3
 for REPO in $REPOS;
 do
-    sudo py2deb -r /home/ubuntu/py2deb -y -- $REPO
+    sudo py2deb -r /home/ubuntu/py2deb3 -y --name-prefix=python3 -- $REPO
 done
 
 # scp /root/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/new/
