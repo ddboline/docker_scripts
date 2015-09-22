da25332d929e3fcba796382f443e9b8bdb36738f
Daniel Boline <ddboline@gmail.com>
2015-09-22 16:47:20 -0400

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# REPOS=git+https://github.com/Lasagne/Lasagne.git \
-#       git+https://github.com/dnouri/nolearn.git
-#       git+https://github.com/lisa-lab/pylearn2.git
-#       
+REPOS="numpy scipy pandas scikit-learn blaze gensim nltk statsmodels pip py2deb
+       py4j requests youtube-dl pysparkling cython pystan seaborn matplotlib 
+       theano Lasagne nolearn scikit-neuralnetwork keras gdbn
+       git+https://github.com/lisa-lab/pylearn2.git"
 
 REPOS="$@"
+if [ -z "$REPOS" ]; then
+    REPOS=""
+fi
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
-sudo apt-get update
-sudo apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
-                               dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
-                               python-setuptools python-py2deb
-# pip install py2deb
-mkdir -p /home/ubuntu/py2deb
+if [ ! -e "/usr/bin/py2deb" ]; then
+    sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
+    sudo apt-get update
+    sudo apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
+                                dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
+                                python-setuptools python-py2deb
+    mkdir -p /home/ubuntu/py2deb
+fi
+
 for REPO in $REPOS;
 do
     py2deb -r /home/ubuntu/py2deb -y -- --upgrade $REPO
