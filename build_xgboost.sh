3910403076572cf675355ca36ecdf96fe52c2f0b
Daniel Boline <ddboline@gmail.com>
2015-09-22 09:57:33 -0400

    ...

--- build_xgboost.sh
+++ build_xgboost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION="0.4.3"
-RELEASE="2"
+VERSION="0.40"
+RELEASE="1"
 REPO="https://github.com/ddboline/xgboost.git"
 
 sudo apt-get update
@@ -18,11 +18,11 @@ sudo checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} -y
 sudo chown ${USER}:${USER} xgboost*.deb
 mv xgboost*.deb ~/py2deb/
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
                                dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
-                               python-setuptools python-py2deb python-numpy python-scipy
+                               python-setuptools python-py2deb python-numpy=1.9\* python-scipy
 mkdir -p /home/ubuntu/py2deb
 cd python-package/
 py2deb -r /home/ubuntu/py2deb -y -- .
