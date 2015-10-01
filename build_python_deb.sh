34b5c531f07e172c0882fc8f5baa69df51091f8a
Daniel Boline <ddboline@gmail.com>
2015-10-01 10:53:55 -0400

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -4,7 +4,8 @@
 
 OPTS="--rename=pyyaml,python-yaml --rename=pyusb,python-usb --rename=websockify,websockify 
       --rename=scikit-learn,python-sklearn --rename=scikit-image,python-skimage 
-      --rename=google-api-python-client,python-googleapi --rename=cython,cython"
+      --rename=google-api-python-client,python-googleapi --rename=cython,cython
+      --rename=pytz,python-tz --rename=pillow,python-pil"
 
 REPOS="py2deb youtube-dl py4j setuptools
        numpy scipy pandas scikit-learn scikit-image
