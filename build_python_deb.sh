f4fd72774507953858661de90e73d7bc12425e54
Daniel Boline <danielb@visibleworld.com>
2015-11-06 15:28:22 -0500

    ...

--- build_python_deb.sh
+++ build_python_deb.sh
@@ -5,7 +5,7 @@ OPTS="--rename=pyyaml,python-yaml --rename=pyusb,python-usb --rename=websockify,
       --rename=google-api-python-client,python-googleapi --rename=cython,cython
       --rename=pytz,python-tz --rename=pillow,python-pil
       --rename=beautifulsoup4,python-bs4 --rename=compose,python-docker-compose
-      --rename=pyzmq,python-zmq"
+      --rename=pyzmq,python-zmq --rename=spyder,spyder"
 
 REPOS="py2deb youtube-dl py4j setuptools docker-compose
        numpy scipy pandas scikit-learn scikit-image
