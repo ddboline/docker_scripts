f001dabf895ece3499b9d970d2324dcba0468020
Daniel Boline <danielb@visibleworld.com>
2015-11-09 12:08:38 -0500

    ...

--- visibleworld/build_universe_api.sh
+++ visibleworld/build_universe_api.sh
@@ -2,7 +2,7 @@
 
 cp /etc/hosts temp.hosts
 echo "10.124.15.33    GitLab02.visibleworld.com" >> temp.hosts
-sudo mv temp.hosts /etc/hosts
+sudo cp temp.hosts /etc/hosts
 
 sudo apt-get update
 sudo apt-get install -y postgresql-server-dev-9.3 npm
