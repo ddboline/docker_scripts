eb3945a725e59cc93d1807fcc286b1dd0bfa215a
Daniel Boline <danielb@visibleworld.com>
2015-11-09 12:03:18 -0500

    ...

--- visibleworld/run_python_deb_build.sh
+++ visibleworld/run_python_deb_build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get install -y postgresql-server-dev-9.3
 sudo apt-get install -y libhdf5-dev
 
@@ -7,3 +9,4 @@ sudo dpkg -i ~/py2deb/python-numpy_*.deb ~/py2deb/cython_*.deb
 sudo apt-get install -f -y --force-yes
 
 ./build_python_deb.sh futures scipy matplotlib spyder sqlalchemy statsmodels tables vcrpy vcrpy-unittest
+./build_python_deb.sh jinja2 pyparsing wheel py2deb
