0c947ba55ceca82dc46b954f439d6362c31c8ba7
Daniel Boline <danielb@visibleworld.com>
2015-11-09 12:06:25 -0500

    ...

--- visibleworld/build_universe_api.sh
+++ visibleworld/build_universe_api.sh
@@ -8,9 +8,10 @@ sudo apt-get update
 sudo apt-get install -y postgresql-server-dev-9.3 npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
+cd ~/
+
 git clone git@GitLab02.visibleworld.com:Data_Analytics/universe_api.git
 sudo npm install -g grunt-cli
-
 
 cd universe_api
 npm install
