4f99fd6a867283739f802cbceca34d73200ffc37
Daniel Boline <ddboline@gmail.com>
2015-11-08 21:03:06 -0500

    ...

--- run_python_deb_build.sh
+++ run_python_deb_build.sh
@@ -7,30 +7,29 @@ cd ~/
 sudo apt-get install -y postgresql-server-dev-9.3
 sudo apt-get install -y libhdf5-dev
 
-./docker_scripts/build_python_deb.sh py2deb youtube-dl py4j setuptools numpy
+./docker_scripts/build_python_deb.sh py2deb youtube-dl py4j 
+
+./docker_scripts/build_python_deb.sh setuptools numpy
 sudo dpkg -i ~/py2deb/python-numpy_*.deb
 sudo apt-get install -f -y --force-yes
-./docker_scripts/build_python_deb.sh scipy pandas matplotlib mock nose pyparsing pbr
-sudo dpkg -i ~/py2deb/python-scipy_*.deb
-sudo apt-get install -f -y --force-yes
-./docker_scripts/build_python_deb.sh scikit-learn scikit-image blaze gensim nltk
-sudo dpkg -i ~/py2deb/python-pandas_*.deb ~/py2deb/python-matplotlib_*.deb \
+
+./docker_scripts/build_python_deb.sh scipy pandas matplotlib mock nose pyparsing pbr python-dateutil pytz cython theano
+sudo dpkg -i ~/py2deb/python-scipy_*.deb ~/py2deb/python-pandas_*.deb ~/py2deb/python-matplotlib_*.deb \
         ~/py2deb/python-mock_*.deb ~/py2deb/python-nose_*.deb ~/py2deb/python-pyparsing_*.deb \
-        ~/py2deb/python-pbr_*.deb ~/py2deb/python-dateutil*.deb ~/py2deb/python-tz*.deb
-sudo apt-get install -f -y --force-yes
-./docker_scripts/build_python_deb.sh statsmodels websockify sharedarray requests pysparkling cython
-sudo dpkg -i ~/py2deb/cython_*.deb
-sudo apt-get install -f -y --force-yes
-./docker_scripts/build_python_deb.sh pystan seaborn theano patsy enum34 executor
-sudo dpkg -i ~/py2deb/python-theano_*.deb 
+        ~/py2deb/python-pbr_*.deb ~/py2deb/python-dateutil*.deb ~/py2deb/python-tz*.deb ~/py2deb/cython_*.deb \
+        ~/py2deb/python-theano_*.deb
 sudo apt-get install -f -y --force-yes
 
+./docker_scripts/build_python_deb.sh scikit-learn scikit-image blaze gensim nltk
+./docker_scripts/build_python_deb.sh statsmodels websockify sharedarray requests pysparkling 
+./docker_scripts/build_python_deb.sh pystan seaborn theano patsy enum34 executor
 ./docker_scripts/build_python_deb.sh lasagne nolearn scikit-neuralnetwork keras gdbn h5py
+
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/pylearn2.git
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/garmin_app.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/roku_app.git \
-                                     git+https://github.com/ddboline/security_log_analysis.git \
-                                     git+https://github.com/ddboline/sync_app.git
+./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/roku_app.git
+./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/security_log_analysis.git
+./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/sync_app.git
 
 OPTS="--rename=pyyaml,python-yaml --rename=pyusb,python-usb --rename=websockify,websockify 
       --rename=scikit-learn,python-sklearn --rename=scikit-image,python-skimage 
@@ -55,17 +54,13 @@ sudo chown ${USER}:${USER} ~/py2deb/*.deb
 ./docker_scripts/build_python_deb.sh asyncio boltons beautifulsoup4 cssselect cytoolz lxml pandasql pillow
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/stravalib.git
 ./docker_scripts/build_python_deb.sh units xray sqlacodegen
-
 ./docker_scripts/build_python_deb.sh fuzzywuzzy spyder pymonad pyquery click pyscaffold
-
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/compose.git
-
 ./docker_scripts/build_python_deb.sh pytest pytest-xdist pytest-instafail getlyrics wget
 
 ./docker_scripts/build_xgboost.sh
 
 ./docker_scripts/build_python_deb.sh ipython coverage openpyxl pika python-logstash jsonschema test-helper
-
 ./docker_scripts/build_python_deb.sh onedrivesdk boxsdk dropbox
 
 scp ~/py2deb/*.deb ddboline@ddbolineathome.mooo.com:~/setup_files/deb/py2deb/py2deb/
