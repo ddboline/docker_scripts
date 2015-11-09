db80e389fe85a54eebb9855cd35741c5d618b034
Daniel Boline <danielb@visibleworld.com>
2015-11-09 10:13:59 -0500

    ...

--- visibleworld/run_python_deb_build.sh
+++ visibleworld/run_python_deb_build.sh
@@ -1,1 +1,9 @@
-./build_python_deb.sh openpyxl pika psycopg2 requests pandas numpy python-logstash jsonschema test-helper
+sudo apt-get install -y postgresql-server-dev-9.3
+sudo apt-get install -y libhdf5-dev
+
+./build_python_deb.sh openpyxl pika psycopg2 requests pandas numpy python-logstash jsonschema test-helper cython
+
+sudo dpkg -i ~/py2deb/python-numpy_*.deb ~/py2deb/cython_*.deb
+sudo apt-get install -f -y --force-yes
+
+./build_python_deb.sh futures scipy matplotlib spyder sqlalchemy statsmodels tables vcrpy vcrpy-unittest
