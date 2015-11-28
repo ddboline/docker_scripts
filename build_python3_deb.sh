32775ffebf4934c132889407e196995eace0ce0d
Daniel Boline <ddboline@gmail.com>
2015-11-27 20:28:46 -0500

    ...

--- build_python3_deb.sh
+++ build_python3_deb.sh
@@ -36,7 +36,7 @@ if [ ! -e "/usr/bin/py2deb" ]; then
     sudo apt-get update
     sudo apt-get install -y --force-yes python3-pip python3-py2deb python3-dev lintian liblapack-dev libblas-dev \
                                         dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
-                                        python3-pkg-resources python3-setuptools
+                                        python3-setuptools
     mkdir -p /home/ubuntu/py2deb3
 fi
 
