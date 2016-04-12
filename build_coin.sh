d7d49f47d368852d9193a06e3dfa76d56c3bb322
Daniel Boline <ddboline@gmail.com>
2016-04-11 21:36:48 -0400

    revert

--- build_coin.sh
+++ build_coin.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-VERSION="2.9.8"
+VERSION="2.9"
 RELEASE="1"
 
 sudo apt-get update
-sudo apt-get install -y git g++ make zlib1g-dev pkg-config \
+sudo apt-get install -y subversion g++ make zlib1g-dev pkg-config \
                         checkinstall
 
-git clone https://github.com/coin-or/Cbc coin_Cbc
+svn co https://projects.coin-or.org/svn/Cbc/stable/${VERSION} coin_Cbc
+
 cd coin_Cbc
-git checkout releases/${VERSION}
-git clone --branch=stable/0.8 https://github.com/coin-or-tools/BuildTools/
-BuildTools/get.dependencies fetch
 
 ./configure --enable-cbc-parallel --prefix=/usr
 make
