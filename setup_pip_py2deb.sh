8109fba90719fa58b02021937af3ceb8d92acfd0
Daniel Boline <ddboline@gmail.com>
2016-06-14 06:43:47 -0400

    ...

---
+++ setup_pip_py2deb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo apt-get update
+sudo apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
+                                        dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
+                                        python-setuptools python-py2deb
+mkdir -p /home/ubuntu/py2deb
+sudo apt-get autoremove && sudo apt-get autoclean && sudo rm -rf /var/cache/apt/archives/*.deb
