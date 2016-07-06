59dcddfb283baaa2b25fab83358297abba7906ff
Daniel Boline <danielb@visibleworld.com>
2016-07-06 09:35:52 -0400

    ...

---
+++ visibleworld/install_mvpd_pronto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm ~/setup_files/deb/py2deb/solver_python/python-mvpd-pronto_*.deb
+mv ~/py2deb/python-mvpd-pronto_*.deb ~/setup_files/deb/py2deb/solver_python/
+sh ~/setup_files/build/ddboline_personal_scripts/setup_python_repo.sh
