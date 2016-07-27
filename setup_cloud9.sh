b8dd961b2e63201b8d54a3320f540c6af5716e17
Daniel Boline <danielb@visibleworld.com>
2016-07-27 14:40:32 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker-engine
 sudo usermod -aG docker ${USER}
 sudo docker login -u ddboline -e ddboline@gmail.com
 sudo docker pull ddboline/ddboline_keys:latest
