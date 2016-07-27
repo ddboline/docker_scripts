341c4a495d38668797466d32563998ffebfc6cd8
Daniel Boline <ddboline@gmail.com>
2016-07-27 16:53:51 -0400

    bugfix

--- run_python_deb_build.sh
+++ run_python_deb_build.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get install -y postgresql-server-dev-9.3 libhdf5-dev libxml2-dev libxslt1-dev
 
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/pip-accel.git@0.43-2
-./docker_scripts/build_python3_deb.sh git+https://github.com/ddboline/python-deb-pkg-tools.git@1.36-1
+./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/python-deb-pkg-tools.git@1.36-1
 
 ### Need recent cython installed to build numpy, need numpy for various other projects...
 ./docker_scripts/build_python_deb.sh cython
@@ -63,7 +63,6 @@ sudo apt-get install -f -y --force-yes
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/security_log_analysis.git
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/sync_app.git
 ./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/movie_collection_app.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/python-deb-pkg-tools.git@1.36-1
 
 OPTS="--rename=pyyaml,python-yaml --rename=pyusb,python-usb --rename=websockify,websockify 
       --rename=scikit-learn,python-sklearn --rename=scikit-image,python-skimage 
