a946cc2d2224724c40d7788eecd9da1bf9d0cce7
Daniel Boline <ddboline@gmail.com>
2016-11-09 05:17:29 -0500

    new version of pip is even more annoying, stopping at 8.1.2 for now

--- run_python3_deb_build.sh
+++ run_python3_deb_build.sh
@@ -7,6 +7,7 @@ cd ~/
 sudo apt-get update
 sudo apt-get install -y postgresql-server-dev-9.3 libhdf5-dev libxml2-dev libxslt1-dev libpython2.7-dev
 
+./docker_scripts/build_python3_deb.sh git+https://github.com/pypa/pip.git@8.1.2
 ./docker_scripts/build_python3_deb.sh git+https://github.com/paylogic/pip-accel.git@pip-8.1-upgrade
 ./docker_scripts/build_python3_deb.sh git+https://github.com/ddboline/python-deb-pkg-tools.git@1.36-1
 
