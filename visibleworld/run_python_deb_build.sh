5f698a59121e033eaba691f227cbc4629c5e7d35
Daniel Boline <danielb@visibleworld.com>
2016-12-09 10:07:42 -0500

    ...

--- visibleworld/run_python_deb_build.sh
+++ visibleworld/run_python_deb_build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install -y postgresql-server-dev-9.5
-sudo apt-get install -y libhdf5-dev libpython2.7-dev
-sudo apt-get install -y freetds-bin freetds-dev
+sudo apt-get update
+sudo apt-get install -y postgresql-server-dev-9.5 libhdf5-dev libxml2-dev \
+                        libxslt1-dev libpython2.7-dev freetds-bin freetds-dev udev
+
 
 ./build_python_deb.sh git+https://github.com/ddboline/cached-property.git@1.3.0.1
 ./build_python_deb.sh git+https://github.com/ddboline/chardet.git@2.3.0.1
 ./build_python_deb.sh git+https://github.com/ddboline/pip-accel.git@pip-8.1-upgrade-1
 ./build_python_deb.sh git+https://github.com/ddboline/python-deb-pkg-tools.git@3.0-1
-./build_python_deb.sh git+https://github.com/ddboline/py2deb.git@0.24.3.1
+./build_python_deb.sh git+https://github.com/ddboline/py2deb.git@0.24.3.2
 
 ./build_python_deb.sh setuptools
 sudo dpkg -i ~/py2deb/python-setuptools_*.deb
@@ -24,6 +25,10 @@ sudo apt-get install -f -y --force-yes
 
 ./build_python_deb.sh git+https://github.com/ddboline/scipy.git@v0.18.1-1
 sudo dpkg -i ~/py2deb/python-scipy_*.deb
+sudo apt-get install -f -y --force-yes
+
+./build_python_deb.sh dateutil pytz cycler
+sudo dpkg -i ~/py2deb/*dateutil*.deb ~/py2deb/*tz*.deb ~/py2deb/*cycler*.deb
 sudo apt-get install -f -y --force-yes
 
 ./build_python_deb.sh git+https://github.com/ddboline/entrypoints.git
