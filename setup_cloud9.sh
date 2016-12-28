f1716a5e76a02c8523d83fe4ce82b3c906e583bb
Daniel Boline <ddboline@gmail.com>
2016-12-28 08:24:41 -0500

    import docker key

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo bash -c "echo deb https://apt.dockerproject.org/repo ubuntu-xenial main > /etc/apt/sources.list.d/docker.list"
-
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv F76221572C52609D
 sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo usermod -aG docker ${USER}
