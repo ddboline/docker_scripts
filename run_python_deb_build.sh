02a90c9145b281ac32dad9adfc8ca45734d1af2b
Daniel Boline <danielb@visibleworld.com>
2017-01-18 11:55:19 -0500

    create run_python_deb_pkgs.txt run_python3_deb_pkgs.txt as list of packages to build

--- run_python_deb_build.sh
+++ run_python_deb_build.sh
@@ -45,53 +45,13 @@ sudo apt-get install -f -y --force-yes
 sudo dpkg -i ~/py2deb/python-entrypoints_*.deb
 sudo apt-get install -f -y --force-yes
 
-./docker_scripts/build_python_deb.sh git+https://github.com/spyder-ide/spyder.git@v2.3.9
-
-./docker_scripts/build_python_deb.sh git+https://github.com/matplotlib/basemap.git
-
-./docker_scripts/build_python_deb.sh statsmodels websockify sharedarray requests pysparkling
-./docker_scripts/build_python_deb.sh pystan seaborn patsy enum34 executor pip
-./docker_scripts/build_python_deb.sh youtube-dl py4j setuptools pytz python-dateutil
-./docker_scripts/build_python_deb.sh scikit-learn scikit-image blaze gensim nltk
-./docker_scripts/build_python_deb.sh lasagne nolearn scikit-neuralnetwork keras gdbn h5py
-./docker_scripts/build_python_deb.sh asyncio boltons beautifulsoup4 cssselect cytoolz lxml pandasql pillow
-./docker_scripts/build_python_deb.sh units xray sqlacodegen
-./docker_scripts/build_python_deb.sh fuzzywuzzy pymonad pyquery click pyscaffold onedrivesdk
-./docker_scripts/build_python_deb.sh pytest pytest-xdist pytest-instafail pyprof2calltree getlyrics wget
-./docker_scripts/build_python_deb.sh ipython coverage openpyxl pika python-logstash jsonschema test-helper
-./docker_scripts/build_python_deb.sh onedrivesdk boxsdk dropbox eventlet pytest-cov
-./docker_scripts/build_python_deb.sh jinja2 nuitka numexpr pexpect tables
-./docker_scripts/build_python_deb.sh deap tpot pyusb imagehash pylint
-./docker_scripts/build_python_deb.sh cloudpickle et_xmlfile flexx greenlet ipython_genutils
-./docker_scripts/build_python_deb.sh jdcal lockfile markupsafe odfpy openpyxl
-./docker_scripts/build_python_deb.sh wheel traitlets simplegeneric pickleshare path.py
-./docker_scripts/build_python_deb.sh pdfkit ptyprocess python-debian pyyaml requests-toolbelt
-./docker_scripts/build_python_deb.sh protobuf protobuf-setuptools setuptools_scm
-./docker_scripts/build_python_deb.sh psycopg2 python-Levenshtein futures boxsdk
-./docker_scripts/build_python_deb.sh records validictory pydrive hypothesis dask retrying
-./docker_scripts/build_python_deb.sh attrs mypy-lang sphinx
-
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/compose.git@1.9.0.1
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/pulp.git@1.6.1-1
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/pylearn2.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/garmin_app.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/roku_app.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/security_log_analysis.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/sync_app.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/movie_collection_app.git
-
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/openant.git
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/antfs-cli.git
-
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/PyFunctional.git@v0.8.0-1
-
-# sudo py2deb -r /home/${USER}/py2deb -y $OPTS -- --upgrade spacy preshed
-# sudo chown ${USER}:${USER} ~/py2deb/*.deb
+for PKG in `cat run_python_deb_pkgs.txt`;
+do
+    ./docker_scripts/build_python_deb.sh $PKG
+done
 
 ./docker_scripts/build_coin.sh
 ./docker_scripts/build_fit2tcx.sh
-
-./docker_scripts/build_python_deb.sh git+https://github.com/ddboline/stravalib.git@0.6.6
 
 ### For the record, I really don't like python packages that depend on f***ing node...
 # ./docker_scripts/build_python_deb.sh jupyter
