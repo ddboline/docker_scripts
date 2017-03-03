deeaccf47789ef3c39bcb673124f87e2358da667
Daniel Boline <ddboline@gmail.com>
2017-03-03 06:22:43 -0500

    overwrite

--- run_python3_deb_build.sh
+++ run_python3_deb_build.sh
@@ -7,7 +7,7 @@ git pull
 cd ~/
 
 sudo apt-get update
-sudo apt-get dist-upgrade -y
+sudo apt-get -o Dpkg::Options::="--force-overwrite" dist-upgrade -y
 sudo apt-get install -y postgresql-server-dev-9.5 libhdf5-dev libxml2-dev libxslt1-dev libpython3.5-dev \
                         freetds-bin freetds-dev udev
 
