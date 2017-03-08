37daceee3577a354b3029edc3cfc303410ad9897
Daniel Boline <ddboline@gmail.com>
2017-03-08 06:20:18 -0500

    ...

--- visibleworld/run_python_deb_build.sh
+++ visibleworld/run_python_deb_build.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y postgresql-server-dev-9.5 libhdf5-dev libxml2-dev \
                         libxslt1-dev libpython2.7-dev freetds-bin freetds-dev udev
 
 ./build_python_deb.sh git+https://github.com/ddboline/packaging.git@16.8-2
-./build_python_deb.sh yarl aiohttp appdirs git+https://github.com/ddboline/cython@0.25.2.1
+./build_python_deb.sh yarl appdirs git+https://github.com/ddboline/cython@0.25.2.1
 sudo dpkg -i ~/py2deb/*.deb
 sudo apt-get install -f -y --force-yes
 
