66736e9aad677390d28be259ed122716729970c8
Daniel Boline <danielb@visibleworld.com>
2017-08-14 13:24:54 -0400

    ...

--- visibleworld/build_python_deb.sh
+++ visibleworld/build_python_deb.sh
@@ -21,7 +21,7 @@ if [ -z "$REPOS" ]; then
 fi
 
 if [ ! -e "/usr/bin/py2deb" ]; then
-    sudo bash -c "echo deb file:///var/www/html/deb/xenial/solver_python/ / > /etc/apt/sources.list.d/py2deb.list"
+    sudo bash -c "echo deb file:///var/www/html/deb/xenial/devel/ / > /etc/apt/sources.list.d/py2deb.list"
     sudo apt-get update
     sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y --force-yes \
                                 python-pip python-dev lintian liblapack-dev libopenblas-dev \
