cae957dee7e5061cda5fe008ab004fcd8f3a7de3
Daniel Boline <ddboline@gmail.com>
2018-01-06 20:36:35 -0500

    ...

--- setup_pip_py2deb.sh
+++ setup_pip_py2deb.sh
@@ -5,8 +5,9 @@ sudo chown -R root:root /root/.ssh
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 25508FAF711C1DEB
 sudo apt-get update
-sudo apt-get install -y --force-yes python-pip python-dev lintian liblapack-dev libblas-dev \
+sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y --force-yes \
+                                        python-pip python-dev lintian liblapack-dev libblas-dev \
                                         dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
-                                        python-setuptools python-py2deb
+                                        python-setuptools python-py2deb libpq-dev
 mkdir -p /home/ubuntu/py2deb
 sudo apt-get autoremove && sudo apt-get autoclean && sudo rm -rf /var/cache/apt/archives/*.deb
--- setup_pip_py2deb3.sh
+++ setup_pip_py2deb3.sh
@@ -5,9 +5,10 @@ sudo chown -R root:root /root/.ssh
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/python3/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 25508FAF711C1DEB
 sudo apt-get update
-sudo apt-get install -y --force-yes python3-pip python3-dev lintian liblapack-dev libblas-dev \
+sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y --force-yes \
+                                        python3-pip python3-dev lintian liblapack-dev libblas-dev \
                                         dpkg-dev gfortran libfreetype6-dev libpng12-dev pkg-config \
-                                        python3-setuptools python3-py2deb
+                                        python3-setuptools python3-py2deb libpq-dev
 mkdir -p /home/ubuntu/py2deb3
 sudo apt-get autoremove && sudo apt-get autoclean && sudo rm -rf /var/cache/apt/archives/*.deb
 sudo rm -rf /usr/lib/python3/dist-packages/pkg_resources
