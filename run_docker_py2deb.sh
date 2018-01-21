7689660ada9e066ff5e8631becd792263bf76f5e
Daniel Boline <ddboline@gmail.com>
2018-01-21 09:06:17 -0500

    ...

---
+++ run_docker_py2deb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ECHO=""
+SUDO=""
+$SUDO echo ""
+D=`date +%Y%m%d%H%M%S`
+TMP="/tmp/tmp.docker.`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 12; echo ;`"
+echo "$@"
+$SUDO docker run --rm -it --name=bash_${D} \
+        ddboline/ddboline_keys:pip_py2deb_latest /home/ubuntu/run_python_deb_build.sh
