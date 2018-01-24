5bb591902a96e3a00caa9fda3fd030f96bf9a953
Daniel Boline <danielb@visibleworld.com>
2018-01-24 07:14:18 -0500

    bugfix

--- visibleworld/run_docker_py2deb.sh
+++ visibleworld/run_docker_py2deb.sh
@@ -7,4 +7,6 @@ D=`date +%Y%m%d%H%M%S`
 TMP="/tmp/tmp.docker.`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 12; echo ;`"
 echo "$@"
 $SUDO docker run --rm --name=bash_${D} \
+        --dns="10.124.5.10" --dns="10.124.5.12" --dns-search="VisibleWorld.com" \
+        -v /var/www/html/deb/xenial:/var/www/html/deb/xenial \
         danielb/danielb_keys:latest /home/ubuntu/docker_scripts/visibleworld/run_python_deb_build.sh
