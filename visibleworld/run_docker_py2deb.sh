764e1c0908ee976b45a78d0a9978767bb9da401e
Daniel Boline <danielb@visibleworld.com>
2018-01-24 06:29:12 -0500

    ...

--- visibleworld/run_docker_py2deb.sh
+++ visibleworld/run_docker_py2deb.sh
@@ -7,4 +7,4 @@ D=`date +%Y%m%d%H%M%S`
 TMP="/tmp/tmp.docker.`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 12; echo ;`"
 echo "$@"
 $SUDO docker run --rm --name=bash_${D} \
-        ddboline/ddboline_keys:pip_py2deb_latest /home/ubuntu/docker_scripts/visibleworld/run_python_deb_build.sh
+        danielb/danielb_keys:latest /home/ubuntu/docker_scripts/visibleworld/run_python_deb_build.sh
