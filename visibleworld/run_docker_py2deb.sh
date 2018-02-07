ef31f9ee97db071013d226009d901a2e5c87b013
Daniel Boline <ddboline@gmail.com>
2018-02-07 06:35:13 -0500

    ...

--- visibleworld/run_docker_py2deb.sh
+++ visibleworld/run_docker_py2deb.sh
@@ -9,4 +9,6 @@ echo "$@"
 $SUDO docker run --rm --name=bash_${D} \
         --dns="10.124.5.10" --dns="10.124.5.12" --dns-search="VisibleWorld.com" \
         -v /var/www/html/deb/xenial:/var/www/html/deb/xenial \
-        danielb/danielb_keys:latest /home/ubuntu/docker_scripts/visibleworld/run_python_deb_build.sh
+        danielb/danielb_keys:latest \
+        /home/ubuntu/docker_scripts/visibleworld/run_python_deb_build.sh \
+        2>&1 > /home/ddboline/setup_files/deb/py2deb/py2deb/solver.log
