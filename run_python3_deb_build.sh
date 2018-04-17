16e5132d57b60887a39c37844be77637d7bb1663
Daniel Boline <ddboline@gmail.com>
2018-04-16 20:26:19 -0400

    ...

--- run_python3_deb_build.sh
+++ run_python3_deb_build.sh
@@ -9,7 +9,7 @@ cd ~/
 sudo apt-get update
 sudo apt-get install -y postgresql-server-dev-9.5 libhdf5-dev libxml2-dev libxslt1-dev libpython3.5-dev \
                         freetds-bin freetds-dev udev libfreetype6-dev libpng12-dev pkg-config \
-                        libgeos++-dev pandoc libmysqlclient-dev
+                        libgeos++-dev pandoc libmysqlclient-dev swig
 
 touch build.log
 md5sum /home/${USER}/py2deb3/*.deb > existing.log
@@ -17,6 +17,7 @@ for PKG in `cat ./docker_scripts/run_python3_deb_pkgs.txt`;
 do
     ./docker_scripts/build_python3_deb.sh $PKG 2>&1 >> build.log
     rm -rf /tmp/pip-*-build
+    rm -rf /home/${USER}/py2deb3/python3-pip_10*.deb
     md5sum /home/${USER}/py2deb3/*.deb > modified.log
     MODIFIED=`diff -u existing.log modified.log | awk '$1 ~ /\+/ && $1 != "+++" {I=I" "$2} END{print I}'`
     sudo dpkg --force-overwrite -i $MODIFIED
--- run_python_deb_build.sh
+++ run_python_deb_build.sh
@@ -9,7 +9,7 @@ cd ~/
 sudo apt-get update
 sudo apt-get install -y postgresql-server-dev-9.5 libhdf5-dev libxml2-dev libxslt1-dev \
                         libpython2.7-dev freetds-bin freetds-dev udev libgeos++-dev pandoc \
-                        libfreetype6-dev libpng12-dev libmysqlclient-dev
+                        libfreetype6-dev libpng12-dev libmysqlclient-dev swig
 
 touch build.log
 md5sum /home/${USER}/py2deb/*.deb > existing.log
@@ -17,6 +17,7 @@ for PKG in `cat ./docker_scripts/run_python_deb_pkgs.txt`;
 do
     ./docker_scripts/build_python_deb.sh $PKG 2>&1 >> build.log
     rm -rf /tmp/pip-*-build
+    rm -rf /home/${USER}/py2deb/python-pip_10*.deb
     md5sum /home/${USER}/py2deb/*.deb > modified.log
     MODIFIED=`diff -u existing.log modified.log | awk '$1 ~ /\+/ && $1 != "+++" {I=I" "$2} END{print I}'`
     sudo dpkg --force-overwrite -i $MODIFIED
