948b99257701739c1dd8f39e1191d913c24d4d63
Daniel Boline <ddboline@gmail.com>
2018-05-09 06:12:08 -0400

    ...

--- visibleworld/run_docker_py2deb.sh
+++ visibleworld/run_docker_py2deb.sh
@@ -6,7 +6,7 @@ $SUDO echo ""
 D=`date +%Y%m%d%H%M%S`
 TMP="/tmp/tmp.docker.`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 12; echo ;`"
 echo "$@"
-$SUDO docker run --rm --name=bash_${D} \
+$SUDO docker run --rm --name=py2deb_${D} \
         --dns="10.124.5.10" --dns="10.124.5.12" --dns-search="VisibleWorld.com" \
         -v /var/www/html/deb/xenial:/var/www/html/deb/xenial \
         danielb/danielb_keys:latest \
