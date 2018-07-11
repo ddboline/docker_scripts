7c271bc23831e06eae9ec8ee17f02916d80d5fe2
Daniel Boline <ddboline@gmail.com>
2018-07-11 05:42:23 -0400

    ...

--- visibleworld/install_mvpd_pronto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm ~/setup_files/deb/py2deb/solver_python/python-mvpd-pronto_*.deb
-mv ~/py2deb/python-mvpd-pronto_*.deb ~/setup_files/deb/py2deb/solver_python/
-~/setup_files/build/ddboline_personal_scripts/setup_python_repo.sh
