51a77aea2807352ab58718dfa5dc4656ffa9828e
Daniel Boline <ddboline@gmail.com>
2018-10-01 07:20:10 -0400

    ...

---
+++ build_efs_utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION="0.4.3"
+RELEASE="2"
+REPO="https://github.com/aws/efs-utils.git"
+
+sudo apt-get update
+sudo apt-get install -y binutils pkg-config \
+                        checkinstall
+
+git clone $REPO
+
+cd efs-utils
+
+make deb
+
+mv build/amazon-efs-utils*.deb ~/py2deb/
