9c88d4f2638e2172fc1ded3e37053769e5125649
Daniel Boline <ddboline@gmail.com>
2019-01-15 11:47:17 -0500

    ...

---
+++ build_movie_collection_rust.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+REPO="https://github.com/ddboline/movie_collection_rust.git"
+
+apt-get update && \
+apt-get install -y curl pkg-config checkinstall gcc libssl-dev ca-certificates \
+        file build-essential autoconf automake autotools-dev libtool xutils-dev \
+        libusb-dev libxml2-dev libpq-dev && \
+rm -rf /var/lib/apt/lists/* && \
+curl https://sh.rustup.rs > rustup.sh && \
+sh rustup.sh -y && \
+. ~/.cargo/env
+
+cd ~/
+
+git clone $REPO movie_collection_rust
+
+cd movie_collection_rust
+
+sh scripts/build_deb_docker.sh
+
+cp movie-collection-rust*.deb ~/py2deb/
+cp movie-collection-rust*.deb ~/py2deb3/
+
+cd ~/
+
+rm -rf ~/movie_collection_rust
