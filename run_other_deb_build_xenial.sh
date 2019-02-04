48fe75eb73b3e707dd96fa143938924c017437eb
Daniel Boline <ddboline@gmail.com>
2019-02-03 22:00:46 -0500

    ...

---
+++ run_other_deb_build_xenial.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/docker_scripts/
+
+git pull
+
+cd ~/
+
+./docker_scripts/build_fit2tcx.sh 2>&1 >> build.log
+./docker_scripts/build_efs_utils.sh 2>&1 >> build.log
+./docker_scripts/build_garmin_rust.sh 2>&1 >> build.log
+./docker_scripts/build_movie_collection_rust.sh 2>&1 >> build.log
+./docker_scripts/build_rust_auth.sh 2>&1 >> build.log
+
+MODIFIED=/home/${USER}/py2deb3/*.deb
+if [ -n "$MODIFIED" ]; then
+    scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb/xenial/py2deb/
+    scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb3/xenial/py2deb3/
+fi
