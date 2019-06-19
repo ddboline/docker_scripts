b15ff55cf8609a4f0c73ecf28fe8f0668c4f03d3
Daniel Boline <ddboline@gmail.com>
2019-06-18 21:42:05 -0400

    ...

---
+++ build_podcatch_rust.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPO="https://github.com/ddboline/podcatch_rust.git"
+
+cd ~/
+
+git clone $REPO podcatch_rust
+
+cd podcatch_rust
+
+make pull && make && make package
+
+cp podcatch-rust*.deb ~/py2deb/
+cp podcatch-rust*.deb ~/py2deb3/
+
+cd ~/
+
+sudo rm -rf ~/podcatch_rust
