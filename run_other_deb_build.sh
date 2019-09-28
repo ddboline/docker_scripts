a10c30a6078aa2bcafd3712e3b4d0e21a7e878d3
Daniel Boline <ddboline@gmail.com>
2019-09-28 13:39:50 +0000

    purge xenial

--- run_other_deb_build.sh
+++ run_other_deb_build.sh
@@ -32,19 +32,3 @@ if [ -n "$MODIFIED" ]; then
     scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb/bionic/py2deb/
     scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb3/bionic/py2deb3/
 fi
-
-rm /home/${USER}/py2deb3/*.deb
-
-./docker_scripts/build_garmin_rust_xenial.sh 2>&1 >> build.log
-./docker_scripts/build_movie_collection_rust_xenial.sh 2>&1 >> build.log
-./docker_scripts/build_rust_auth_xenial.sh 2>&1 >> build.log
-./docker_scripts/build_sync_app_rust_xenial.sh 2>&1 >> build.log
-./docker_scripts/build_podcatch_rust_xenial.sh 2>&1 >> build.log
-./docker_scripts/build_security_log_analysis_rust_xenial.sh 2>&1 >> build.log
-./docker_scripts/build_diary_app_rust_xenial.sh 2>&1 >> build.log
-
-MODIFIED=/home/${USER}/py2deb3/*.deb
-if [ -n "$MODIFIED" ]; then
-    scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb/xenial/py2deb/
-    scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb3/xenial/py2deb3/
-fi
