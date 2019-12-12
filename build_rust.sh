db0af99ccb702c429272cc1b0a835de88b516359
Daniel Boline <ddboline@gmail.com>
2019-12-12 16:38:42 +0000

    ...

--- build_rust.sh
+++ build_rust.sh
@@ -23,8 +23,9 @@ for PKGS in "fd-find,fd,fd" \
     "diesel_cli,diesel,diesel-cli" \
     "cargo-generate,cargo-generate,cargo-generate" \
     "cargo-tree,cargo-tree,cargo-tree" \
-    "cargo-outdated,cargo-outdated,cargo-outdated"
-    "cargo-edit,cargo-edit,cargo-edit";
+    "cargo-outdated,cargo-outdated,cargo-outdated" \
+    "cargo-edit,cargo-edit,cargo-edit" \
+    "cargo-scout,cargo-scout,cargo-scout";
 do
     CARGO=`echo $PKGS | sed 's:,: :g' | awk '{print $1}'`;
     EXE=`echo $PKGS | sed 's:,: :g' | awk '{print $2}'`;
