eebf7f11b796191824cb253aaa205a429e68306d
Daniel Boline <ddboline@gmail.com>
2019-12-30 15:22:00 -0500

    add what

--- build_rust.sh
+++ build_rust.sh
@@ -25,7 +25,8 @@ for PKGS in "fd-find,fd,fd" \
     "cargo-tree,cargo-tree,cargo-tree" \
     "cargo-outdated,cargo-outdated,cargo-outdated" \
     "cargo-edit,cargo-edit,cargo-edit" \
-    "cargo-scout,cargo-scout,cargo-scout";
+    "cargo-scout,cargo-scout,cargo-scout" \
+    "what,what,what";
 do
     CARGO=`echo $PKGS | sed 's:,: :g' | awk '{print $1}'`;
     EXE=`echo $PKGS | sed 's:,: :g' | awk '{print $2}'`;
