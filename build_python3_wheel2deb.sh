640642f27a3351d01ac7bf6d5c08be3136065362
Daniel Boline <ddboline@gmail.com>
2020-05-07 13:31:04 -0400

    noninteractive

---
+++ build_python3_wheel2deb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+OPTS="pyyaml=python3-yaml pyusb=python3-usb websockify=websockify3 
+      scikit-learn=python3-sklearn scikit-image=python3-skimage 
+      google-api-python-client=python3-googleapi cython=cython3
+      pytz=python3-tz pillow=python3-pil python-debian=python3-debian
+      beautifulsoup4=python3-bs4 compose=python3-docker-compose
+      pyzmq=python3-zmq spyder=spyder3 ipython=ipython3
+      pylint=pylint3 pyflakes=pyflakes3 python-dateutil=python3-dateutil"
+
+### hack...
+export LANG="C.UTF-8"
+
+REPOS="$@"
+if [ -z "$REPOS" ]; then
+    REPOS=""
+fi
+
+if [ ! -e "/usr/bin/py2deb" ]; then
+    sudo bash -c "echo deb [trusted=yes] https://py2deb-repo.s3.amazonaws.com/deb/focal/python3 focal main > /etc/apt/sources.list.d/py2deb3.list"
+
+    sudo apt-get update
+    sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y --force-yes \
+                                python3-pip python3-wheel2deb python3-dev lintian liblapack-dev libblas-dev \
+                                dpkg-dev gfortran libfreetype6-dev libpng-dev pkg-config \
+                                python3-setuptools libsnappy-dev
+    mkdir -p /home/${USER}/output
+fi
+
+md5sum /home/${USER}/output/*.deb > existing.log
+
+for REPO in $REPOS;
+do
+    pip3 install $REPO
+    pip3 wheel $REPO
+    wheel2deb --map $OPTS
+    wheel2deb build
+    wheel2deb --map $OPTS --ignore-entry-points
+    wheel2deb build
+done
---
+++ run_python3_wheel2deb_build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd ~/docker_scripts/
+
+git pull
+
+cd ~/
+
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get \
+    -o Dpkg::Options::=--force-confold \
+    -o Dpkg::Options::=--force-confdef \
+    -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+    install -yq postgresql-server-dev-12 libhdf5-dev libxml2-dev libxslt1-dev libpython3.8-dev \
+                freetds-bin freetds-dev udev libfreetype6-dev libpng-dev pkg-config \
+                libgeos-dev pandoc libmysqlclient-dev swig librdkafka-dev llvm-10 \
+                libproj-dev proj-bin
+
+sudo ln -s /usr/bin/llvm-config-10 /usr/bin/llvm-config
+
+touch build.log
+md5sum /home/${USER}/output/*.deb > existing.log
+for PKG in `cat ./docker_scripts/run_python3_deb_pkgs.txt`;
+do
+    ./docker_scripts/build_python3_wheel2deb.sh $PKG 2>&1 >> build.log
+    rm -rf /tmp/pip-*-build
+    md5sum /home/${USER}/output/*.deb > modified.log
+    MODIFIED=`diff -u existing.log modified.log | awk '$1 ~ /\+/ && $1 != "+++" {I=I" "$2} END{print I}'`
+    sudo sed -i 's/numpy (<< 1:/numpy (<< /g' /var/lib/dpkg/status
+    sudo dpkg --force-overwrite -i $MODIFIED
+    sudo apt-get -o Dpkg::Options::="--force-overwrite" install -f -y --force-yes
+    mv modified.log existing.log
+done
+
+MODIFIED=/home/${USER}/output/*.deb
+if [ -n "$MODIFIED" ]; then
+    scp $MODIFIED build.log ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb3/focal/py2deb3/
+fi
