0a66067f0f4d197b2641db9c0ae0de15e3c7eefa
Daniel Boline <ddboline@gmail.com>
2020-05-07 13:43:24 -0400

    ...

--- build_python3_wheel2deb.sh
+++ build_python3_wheel2deb.sh
@@ -16,14 +16,20 @@ if [ -z "$REPOS" ]; then
     REPOS=""
 fi
 
-if [ ! -e "/usr/bin/py2deb" ]; then
-    sudo bash -c "echo deb [trusted=yes] https://py2deb-repo.s3.amazonaws.com/deb/focal/python3 focal main > /etc/apt/sources.list.d/py2deb3.list"
-
+if [ ! -e "/usr/bin/wheel2deb" ]; then
+#     sudo bash -c "echo deb [trusted=yes] https://py2deb-repo.s3.amazonaws.com/deb/focal/python3 focal main > /etc/apt/sources.list.d/py2deb3.list"
+# 
+#     sudo apt-get update
+#     sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y --force-yes \
+#                                 python3-pip python3-wheel2deb python3-dev lintian liblapack-dev libblas-dev \
+#                                 dpkg-dev gfortran libfreetype6-dev libpng-dev pkg-config \
+#                                 python3-setuptools libsnappy-dev
     sudo apt-get update
     sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y --force-yes \
-                                python3-pip python3-wheel2deb python3-dev lintian liblapack-dev libblas-dev \
+                                python3-pip python3-dev lintian liblapack-dev libblas-dev \
                                 dpkg-dev gfortran libfreetype6-dev libpng-dev pkg-config \
                                 python3-setuptools libsnappy-dev
+    sudo pip3 install wheel2deb
     mkdir -p /home/${USER}/output
 fi
 
