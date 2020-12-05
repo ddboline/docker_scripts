0379d6b28dbf102a7b1f2c0455cd7e734f27d647
Daniel Boline <ddboline@gmail.com>
2020-12-05 09:09:58 -0500

    build new if possible, otherwise build update

--- build_rust_docker.sh
+++ build_rust_docker.sh
@@ -4,7 +4,7 @@ mkdir -p ~/py2deb3
 
 cd ~/docker_scripts/rust_nightly
 
-make && make push
+(make && make push) || (make pull && make update && make push)
 
 docker rmi -f `docker images | awk '/rust_nightly/ {print $3}' | sort | uniq`
 
