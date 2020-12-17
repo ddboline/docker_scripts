7515aad34b3c50f6590fa6d40ed45439dae22d27
Daniel Boline <ddboline@gmail.com>
2020-12-16 21:42:13 -0500

    use cargo install directly

--- rust_stable/build_rust_pkg_repo.sh
+++ rust_stable/build_rust_pkg_repo.sh
@@ -3,29 +3,11 @@
 . ~/.cargo/env
 
 REPO_URL=$1
-EXE_NAME=$2
-PACKAGE_NAME=$3
+PACKAGE_NAME=$2
 
-echo $CARGO_NAME $EXE_NAME $PACKAGE_NAME $VERSION $RELEASE
+echo $REPO_URL $PACKAGE_NAME
 
-mkdir -p ~/${PACKAGE_NAME}
-cd ~/${PACKAGE_NAME}
-
-cargo install $PACKAGE_NAME --git $REPO_URL
-
-
-git clone $REPO_URL $PACKAGE_NAME/
-
-cd $PACKAGE_NAME/
-
-VERSION=`awk '/^version/' Cargo.toml | head -n1 | cut -d "=" -f 2 | sed 's: ::g'`
-RELEASE="1"
-
-echo $CARGO_NAME $EXE_NAME $PACKAGE_NAME $VERSION $RELEASE
-
-cargo build --release
-
-printf "\ninstall:\n\tcp ~/${PACKAGE_NAME}/target/release/${EXE_NAME} /usr/bin/\n" > Makefile
+printf "\ninstall:\n\tcargo install ${PACKAGE_NAME} --git ${REPO_URL} --root=/usr"
 printf "${PACKAGE_NAME} package\n" > description-pak
 checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} --pkgname ${PACKAGE_NAME} -y
 chown ${USER}:${USER} ${PACKAGE_NAME}_${VERSION}-${RELEASE}*.deb
