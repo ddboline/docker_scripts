e743e633ac4e4cd6a2925d2f93fca21de5c4319e
Daniel Boline <ddboline@gmail.com>
2020-12-17 07:38:43 -0500

    bugfix

--- rust_stable/build_rust_pkg_repo.sh
+++ rust_stable/build_rust_pkg_repo.sh
@@ -7,7 +7,7 @@ PACKAGE_NAME=$2
 
 echo $REPO_URL $PACKAGE_NAME
 
-printf "\ninstall:\n\tcargo install ${PACKAGE_NAME} --git ${REPO_URL} --root=/usr"
+printf "\ninstall:\n\tcargo install ${PACKAGE_NAME} --git ${REPO_URL} --root=/usr" > Makefile
 printf "${PACKAGE_NAME} package\n" > description-pak
 checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} --pkgname ${PACKAGE_NAME} -y
 chown ${USER}:${USER} ${PACKAGE_NAME}_${VERSION}-${RELEASE}*.deb
