9b10999e17933fae6bdcdbf3f99c07be0031ac83
Daniel Boline <ddboline@gmail.com>
2020-12-17 19:41:10 -0500

    ...

--- rust_stable/build_rust_pkg_repo.sh
+++ rust_stable/build_rust_pkg_repo.sh
@@ -5,10 +5,11 @@
 REPO_URL=$1
 CRATE_NAME=$2
 PACKAGE_NAME=$3
+BRANCH_NAME=$4
 
-echo $REPO_URL $CRATE_NAME $PACKAGE_NAME
+echo $REPO_URL $CRATE_NAME $PACKAGE_NAME $BRANCH_NAME
 
-printf "\ninstall:\n\tcargo install ${CRATE_NAME} --git ${REPO_URL} --root=/usr" > Makefile
+printf "\ninstall:\n\tcargo install ${CRATE_NAME} --git=${REPO_URL} --branch=${BRANCH_NAME} --root=/usr" > Makefile
 printf "${PACKAGE_NAME} package\n" > description-pak
 checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} --pkgname ${PACKAGE_NAME} -y
 chown ${USER}:${USER} ${PACKAGE_NAME}_${VERSION}-${RELEASE}*.deb
