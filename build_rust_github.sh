0d2363c2245e39e3c095195bf91f791a9d564445
Daniel Boline <ddboline@gmail.com>
2021-01-21 18:08:23 -0500

    more dedockerify

--- build_rust_github.sh
+++ build_rust_github.sh
@@ -2,12 +2,21 @@
 
 mkdir -p ~/py2deb3
 
-export AWS_ACCOUNT=$(aws sts get-caller-identity | awk '/Account/ {print $2}' | sed 's:[^0-9]::g')
-
-`aws ecr --region us-east-1 get-login --no-include-email`
-docker pull ${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/rust_stable:latest
-docker tag ${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/rust_stable:latest rust_stable:latest
-docker rmi ${AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/rust_stable:latest
+sudo apt-get update && \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+            curl pkg-config checkinstall gcc libssl-dev ca-certificates \
+            file build-essential autoconf automake autotools-dev libtool xutils-dev \
+            git libusb-dev libxml2-dev libpq-dev libpython3.8-dev llvm clang \
+            default-libmysqlclient-dev libsqlite3-dev libsodium-dev libclang-dev \
+            nettle-dev libxcb1-dev libxcb-render0-dev libxcb-shape0-dev \
+            libxcb-xfixes0-dev && \
+    sudo rm -rf /var/lib/apt/lists/* && \
+    curl https://sh.rustup.rs > rustup.sh && \
+    sh rustup.sh -y && \
+    . ~/.cargo/env && \
+    rustup component add rustfmt && \
+    rustup component add clippy && \
+    cargo install cargo-deb
 
 REPONAME=$1
 
@@ -15,15 +24,22 @@ REPO="https://github.com/ddboline/${REPONAME}.git"
 
 cd ~/
 
-git clone $REPO ${REPONAME}
+git clone $REPO ~/$REPONAME/
 
-cd ${REPONAME}
+cd ~/$REPONAME/
 
-docker run --rm -v ~/py2deb3:/root/py2deb3 rust_stable:latest /root/build_rust_pkg_deb.sh ${REPO} ${REPONAME}
+VERSION=`awk '/^version/' Cargo.toml | head -n1 | cut -d "=" -f 2 | sed 's: ::g'`
+RELEASE="1"
+
+echo $CARGO_NAME $REPONAME $VERSION $RELEASE
+
+cargo deb
+
+mv ~/${REPONAME}/target/debian/*.deb ~/py2deb3/
+
 sudo chown ${USER}:${USER} ~/py2deb3/*.deb
 
 scp ~/py2deb3/*.deb ubuntu@cloud.ddboline.net:/home/ubuntu/setup_files/deb/py2deb3/focal/devel_rust/
+rm ~/py2deb3/*.deb
 
-cd ~/
-
-sudo rm -rf ~/${REPONAME}
+rm -rf ~/${REPONAME}
