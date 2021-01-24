dc207344dec41ae38d97c633a5da2e0930890e0a
Daniel Boline <ddboline@gmail.com>
2021-01-24 10:09:14 -0500

    typo

--- build_rust.sh
+++ build_rust.sh
@@ -90,7 +90,7 @@ do
     CARGO=`echo $PKG | sed 's:,: :g' | awk '{print $1}'`;
     EXE=`echo $PKG | sed 's:,: :g' | awk '{print $2}'`;
     PACKAGE=`echo $PKG | sed 's:,: :g' | awk '{print $3}'`;
-    VERSION=`cargo search $CARGO_NAME 2> /dev/null | head -n1 | awk '{print $3}' | sed 's:"::g'`
+    VERSION=`cargo search $CARGO 2> /dev/null | head -n1 | awk '{print $3}' | sed 's:"::g'`
     RELEASE="1"
 
     echo $CARGO $EXE $PACKAGE $VERSION $RELEASE
