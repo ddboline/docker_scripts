a60f785dbde0494765fd8db5fb54aeb0c774efd0
Daniel Boline <ddboline@gmail.com>
2021-11-03 07:03:08 -0400

    stop building amazon rust docker image

--- build_rust_docker.sh
+++ build_rust_docker.sh
@@ -9,9 +9,3 @@ cd ~/docker_scripts/rust_nightly
 docker rmi -f `docker images | awk '/rust_nightly/ {print $3}' | sort | uniq`
 
 cd ~/docker_scripts/rust_stable
-
-make amazon && make push_amazon
-
-docker rmi -f `docker images | awk '/rust_stable/ && /amazon/ {print $3}' | sort | uniq`
-
-make && make push
