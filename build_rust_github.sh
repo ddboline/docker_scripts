2639e5e1d69248618a8ad6fd4a1890f349e23bbd
Daniel Boline <ddboline@gmail.com>
2022-06-20 20:37:53 -0400

    bugfix

--- build_rust_github.sh
+++ build_rust_github.sh
@@ -2,9 +2,10 @@
 
 mkdir -p ~/py2deb3
 
+# libssl-dev
 sudo apt-get update && \
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
-            curl pkg-config checkinstall gcc ca-certificates \ # libssl-dev
+            curl pkg-config checkinstall gcc ca-certificates \
             file build-essential autoconf automake autotools-dev libtool xutils-dev \
             git libusb-dev libxml2-dev libpq-dev libpython3-dev llvm clang \
             default-libmysqlclient-dev libsqlite3-dev libsodium-dev libclang-dev \
